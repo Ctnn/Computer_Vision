e149bf95fdb72f993fdbcd60425324080e21c867
Ctnn <yigitcetinkaya2002@hotmail.com>
2022-08-04 17:02:32 +0300

    Convolutions, Blurring and Sharpening Images comp.

---
+++ 8_Convolutions_Blurring_Sharpening/convolutionsandmore.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Define our imshow function
+def imshow(title = "Image", image = None, size = 10):
+    w, h = image.shape[0], image.shape[1]
+    aspect_ratio = w/h
+    plt.figure(figsize=(size * aspect_ratio,size))
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.title(title)
+    plt.show()
+
+
+import cv2
+import numpy as np
+
+image = cv2.imread('../publicSource/images/flowers.jpeg')
+imshow('Original Image', image)
+
+# Creating our 3 x 3 kernel
+kernel_3x3 = np.ones((3, 3), np.float32) / 9
+
+# We use the cv2.fitler2D to conovlve the kernal with an image
+blurred = cv2.filter2D(image, -1, kernel_3x3)
+imshow('3x3 Kernel Blurring', blurred)
+
+# Creating our 7 x 7 kernel
+kernel_7x7 = np.ones((7, 7), np.float32) / 49
+
+blurred2 = cv2.filter2D(image, -1, kernel_7x7)
+imshow('7x7 Kernel Blurring', blurred2)
