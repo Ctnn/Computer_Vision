9e4579025d47012d27194127890228996f58a689
Ctnn <yigitcetinkaya2002@hotmail.com>
2022-08-05 12:30:28 +0300

    Countours comp.

--- 11_Contours/contours.py
+++ 11_Contours/contours.py
@@ -32,3 +32,55 @@ imshow('Contours overlaid on original image', image)
 print("Number of Contours found = " + str(len(contours)))
 
 
+# Convert to Grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+_, th2 = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+imshow('After thresholding', th2)
+
+# Use a copy of your image e.g. edged.copy(), since findContours alters the image
+contours, hierarchy = cv2.findContours(th2, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+
+# Draw all contours, note this overwrites the input image (inplace operation)
+# Use '-1' as the 3rd parameter to draw all
+cv2.drawContours(image, contours, -1, (0,255,0), thickness = 2)
+imshow('Contours overlaid on original image', image)
+
+print("Number of Contours found = " + str(len(contours)))
+print(hierarchy)
+
+# Convert to Grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+_, th2 = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+imshow('After thresholding', th2)
+
+# Use a copy of your image e.g. edged.copy(), since findContours alters the image
+contours, hierarchy = cv2.findContours(th2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+# Draw all contours, note this overwrites the input image (inplace operation)
+# Use '-1' as the 3rd parameter to draw all
+cv2.drawContours(image, contours, -1, (0,255,0), thickness = 2)
+imshow('Contours overlaid on original image', image)
+
+print("Number of Contours found = " + str(len(contours)))
+for c in contours:
+  print(len(c))
+
+  # Convert to Grayscale
+  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+  _, th2 = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+  imshow('After thresholding', th2)
+
+  # Use a copy of your image e.g. edged.copy(), since findContours alters the image
+  contours, hierarchy = cv2.findContours(th2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+  # Draw all contours, note this overwrites the input image (inplace operation)
+  # Use '-1' as the 3rd parameter to draw all
+  cv2.drawContours(image, contours, -1, (0, 255, 0), thickness=2)
+  imshow('Contours overlaid on original image', image)
+
+  print("Number of Contours found = " + str(len(contours)))
+  for c in contours:
+      print(len(c))
