d0f0555b9c347bdca70f32a5e183d6b6094c4c0e
Ctnn <yigitcetinkaya2002@hotmail.com>
2022-08-07 13:13:06 +0300

    KMeans clustering to obtain the dominant colors in an image

---
+++ 19_HistogramsAndKMeansClusteringForDominantColor/k_means_uses.py
@@ -0,0 +1,63 @@
+def centroidHistogram(clt):
+    # Create a histrogram for the clusters based on the pixels in each cluster
+    # Get the labels for each cluster
+    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+
+    # Create our histogram
+    (hist, _) = np.histogram(clt.labels_, bins = numLabels)
+
+    # normalize the histogram, so that it sums to one
+    hist = hist.astype("float")
+    hist /= hist.sum()
+
+    return hist
+
+def plotColors(hist, centroids):
+    # Create our blank barchart
+    bar = np.zeros((100, 500, 3), dtype = "uint8")
+
+    x_start = 0
+    # iterate over the percentage and dominant color of each cluster
+    for (percent, color) in zip(hist, centroids):
+      # plot the relative percentage of each cluster
+      end = x_start + (percent * 500)
+      cv2.rectangle(bar, (int(x_start), 0), (int(end), 100),
+        color.astype("uint8").tolist(), -1)
+      x_start = end
+    return bar
+
+from sklearn.cluster import KMeans
+import cv2
+from matplotlib import pyplot as plt
+
+def imshow(text="Text",image=None,size=14):
+    w,h = image.shape[:2]
+    aspect_ratio=w/h
+    plt.figure(figsize=(size*aspect_ratio,size))
+    plt.imshow(cv2.cvtColor(image,cv2.COLOR_BGR2RGB))
+    plt.title(text)
+    plt.show()
+
+
+image = cv2.imread('images/tobago.jpg')
+imshow("Input", image)
+
+
+# We reshape our image into a list of RGB pixels
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+print(image.shape)
+image = image.reshape((image.shape[0] * image.shape[1], 3))
+print(image.shape)
+
+number_of_clusters = 5
+clt = KMeans(number_of_clusters)
+clt.fit(image)
+
+hist = centroidHistogram(clt)
+bar = plotColors(hist, clt.cluster_centers_)
+
+# show our color bart
+plt.figure()
+plt.axis("off")
+plt.imshow(bar)
+plt.show()
---
+++ 19_HistogramsAndKMeansClusteringForDominantColor/viewedseparetecolor.py
@@ -0,0 +1,33 @@
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def imshow(text="Text",image=None,size=14):
+    w,h = image.shape[:2]
+    aspect_ratio=w/h
+    plt.figure(figsize=(size*aspect_ratio,size))
+    plt.imshow(cv2.cvtColor(image,cv2.COLOR_BGR2RGB))
+    plt.title(text)
+    plt.show()
+
+
+image=cv2.imread('../publicSource/images/tobago.jpg')
+imshow("Input",image)
+
+histogram = cv2.calcHist([image], [0], None, [256], [0, 256])
+
+# We plot a histogram, ravel() flatens our image array
+plt.hist(image.ravel(), 256, [0, 256]);
+plt.show()
+
+# Viewing Separate Color Channels
+color = ('b', 'g', 'r')
+
+# We now separate the colors and plot each in the Histogram
+for i, col in enumerate(color):
+    histogram2 = cv2.calcHist([image], [i], None, [256], [0, 256])
+    plt.plot(histogram2, color=col)
+    plt.xlim([0, 256])
+
+plt.show()
