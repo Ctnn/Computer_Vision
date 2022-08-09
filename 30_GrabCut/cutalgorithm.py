0768ecc85387ba95155278713a604f3bd38e2a8b
Ctnn <yigitcetinkaya2002@hotmail.com>
2022-08-09 09:08:39 +0300

    Morning commit

---
+++ 30_GrabCut/cutalgorithm.py
@@ -0,0 +1,14 @@
+import cv2
+import dlib
+import sys
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Define our imshow function 
+def imshow(title = "Image", image = None, size = 10):
+    w, h = image.shape[0], image.shape[1]
+    aspect_ratio = w/h
+    plt.figure(figsize=(size * aspect_ratio,size))
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.title(title)
+    plt.show()
